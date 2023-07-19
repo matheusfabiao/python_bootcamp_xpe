df5c5ba7a8c051b12cf65381188d6d63079ceb2c
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-07-19 14:37:45 -0300

    aula de variaveis concluída

---
+++ variaveis.py
@@ -0,0 +1,20 @@
+# A criação de uma variável é realizada por meio da declaração e a atribuição de um valor à ela:
+# variavel = valor
+
+pessoa = "Matheus Fabiao"
+idade1 = 22
+altura = 1.74
+
+print(pessoa, idade1, altura)
+
+texto = "Este é um texto grande!"
+porcentagem = 0.5
+numero = -10
+
+print(texto, porcentagem, numero)
+
+idade2 = 2023 - 2001
+print(idade2)
+
+decimal = 15/4
+print(decimal)
