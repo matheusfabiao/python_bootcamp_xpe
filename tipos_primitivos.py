9f5eeb08eb4a72c7f11fae4b998939bffba3eefb
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-07-19 15:17:56 -0300

    aula de tipos de dados concluída

---
+++ tipos_primitivos.py
@@ -0,0 +1,18 @@
+# Tipos primitivos de Dados:
+"""
+int -> 0, 99, -3, -87
+float -> 3.5, -9.1, 3.141592 (pi)
+bool -> True, False
+str -> "a", 'São Paulo', "céu"
+None -> valor nulo
+"""
+a = 3
+b = 3.0
+c = True
+d = "Matheus Fabiao"
+j = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(j))
