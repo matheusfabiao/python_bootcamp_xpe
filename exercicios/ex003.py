04a28918d987974083e63f1d5c524dcb0440ad92
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-07-29 10:52:19 -0300

    exercício 03 concluído

---
+++ exercicios/ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+res = n1 + n2
+print(res)
