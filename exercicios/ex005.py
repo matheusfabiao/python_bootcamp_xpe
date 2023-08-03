d5eeaeb6be598cc267a245363c95de588d78b8f7
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-02 23:19:10 -0300

    exercício 5 concluído

---
+++ exercicios/ex005.py
@@ -0,0 +1,5 @@
+"""
+Escreva um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+"""
+num = int(input('Digite um número: '))
+print(f'O número digitado foi {num}, seu antecessor é {num - 1} e seu sucessor é {num + 1}')
