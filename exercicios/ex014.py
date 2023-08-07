9506940178244f54629c4cdd1db9550773e3f71f
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-08-07 00:33:17 -0300

    exercício 14 concluído

---
+++ exercicios/ex014.py
@@ -0,0 +1,6 @@
+"""
+Escreva um programa que converta uma temperatura digitada em °C e converta para °F
+"""
+tempC = int(input('Digite a temperatura em °C: '))
+tempF = (1.8 * tempC) + 32
+print(f'Temperatura em °C: {tempC}°C\nTemperatura em °F: {tempF}°F')
