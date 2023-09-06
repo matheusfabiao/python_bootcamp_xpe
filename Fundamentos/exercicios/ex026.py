955c64b12e8166174b481e1ce6c721b15ddcc1ec
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-09-06 07:18:19 -0300

    ex026 finalizado

--- Fundamentos/exercicios/ex026.py
+++ Fundamentos/exercicios/ex026.py
@@ -1,3 +1,9 @@
+"""
+Faça um programa que leia uma frase pelo teclado e mostre:
+1. Quantas vezes aparace a letra "A"
+2. Em que posição ela aparece pela primeira vez
+3. Em que posição ela aparece pela última vez
+"""
 frase = str(input('Digite uma frase: ')).strip().upper()
 print(f'A letra "A" aparece {frase.count("A")} vezes')
 print(f'A primeira letra "A" aparece na posição {frase.find("A") + 1}')
