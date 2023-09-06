77522c55134839bb46ac844a75900617f6525437
Matheus Fabião <matheusfabiaopereira@gmail.com>
2023-09-06 08:01:40 -0300

    ex030 finalizado

---
+++ Fundamentos/exercicios/ex030.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que leia um número inteiro
+e mostre se ele é par ou ímpar
+"""
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+    print(f'O número {num} é par!')
+else:
+    print(f'O número {num} é ímpar!')
